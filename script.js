db6fbfcc35d5d67a43b0ac324c4ae6f49d27bb72
nven <neven.zdelar@gmail.com>
2018-11-08 19:37:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 $(document).ready(function(){
 
-    $(".product").ready(function productController() {
-        var mainGridRowCount = 3;
-        var mainGridColumnCount = 3;
+    $(".z0id0").ready(function productController() {
+        var mainGridRowCount = 10;
+        var mainGridColumnCount = 10;
+        var selectedElement;
+        createGrid()
 
         $( "#backColorBlack" ).click(function() {
-            changeBackgroundColor('black');
+            changeBackgroundColor(selectedElement, 'black');
         });
         $( "#backColorWhite" ).click(function() {
-            changeBackgroundColor('white');
+            changeBackgroundColor(selectedElement, 'white');
         });
         $( "#backColorGrey" ).click(function() {
-            changeBackgroundColor('grey');
+            console.log(this);
+            changeBackgroundColor(selectedElement, 'grey');
         });
-
-        createGrid()
         
-    });
-    //$("#backColorBlack").clicked(changeBackgroundColor('white'));
 
-    function changeBackgroundColor(color) {
-        $(".ly0id0").get(0).style.setProperty("--backColor", color);
-    }
+        $(".z0id0 div").click(function(){
+            selectedElement = this.id;
+            console.log(selectedElement);
+        })
 
-    function createGrid() {
-        $(".ly0id0").get(0).style.setProperty("grid-template-columns", "1fr 1fr 1fr");
-        $(".ly0id0").get(0).style.setProperty("grid-template-rows", "1fr 1fr 1fr");
-        for (var r = 1; r<=3; r++) {
-            for (var c = 1; c<=3; c++) {
-                $(".ly0id0").append("<div id='"+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+";'>aa</div>")
+        function changeBackgroundColor(element, color) {
+            console.log();
+            $("#"+element).get(0).style.setProperty("background-color", color);
+        }
+        /*
+        dobiti broj r i c i dobiti element
+        generirati
+        */
+        function createGrid() {
+    
+            $(".z0id0").get(0).style.setProperty("grid-template-columns", mainGridColumnCount*"1fr ");
+            $(".z0id0").get(0).style.setProperty("grid-template-rows", mainGridRowCount*"1fr ");
+            for (var r = 1; r<=mainGridRowCount; r++) {
+                for (var c = 1; c<=mainGridColumnCount; c++) {
+                    $(".z0id0").append("<div id='"+'z1id'+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+";'>aa</div>")
+                }
             }
+    
         }
 
-    }
+        
+        
+    });
+
+
 
     
 });
