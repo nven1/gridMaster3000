ae6a3df8b8ad6eef92f624589aca13c6fd04e6d9
nven <neven.zdelar@gmail.com>
2018-11-08 19:05:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-
-
 $(document).ready(function(){
-  $("p").click(function(){
-      $(this).hide();
-  });
 
-  $("#slider").bind('input', sliderChange);
-  $("#textSlider").bind('input', sliderChange);
-//slider.addEventListener('input', sliderChange);
+    $(".product").ready(function productController() {
+        var mainGridRowCount = 3;
+        var mainGridColumnCount = 3;
 
-function sliderChange() {
-  $("#textSlider").val(this.value);
-  $("#slider").val(this.value);
-}
+        $( "#backColorBlack" ).click(function() {
+            changeBackgroundColor('black');
+        });
+        $( "#backColorWhite" ).click(function() {
+            changeBackgroundColor('white');
+        });
+        $( "#backColorGrey" ).click(function() {
+            changeBackgroundColor('grey');
+        });
+
+        createGrid()
+        
+    });
+    //$("#backColorBlack").clicked(changeBackgroundColor('white'));
+
+    function changeBackgroundColor(color) {
+        $(".ly0id0").get(0).style.setProperty("--backColor", color);
+    }
+
+    function createGrid() {
+        $(".ly0id0").get(0).style.setProperty("grid-template-columns", "1fr 1fr 1fr");
+        $(".ly0id0").get(0).style.setProperty("grid-template-rows", "1fr 1fr 1fr");
+        for (var r = 1; r<=3; r++) {
+            for (var c = 1; c<=3; c++) {
+                $(".ly0id0").append("<div id='"+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+";'>aa</div>")
+            }
+        }
+
+    }
+
+    
 });
