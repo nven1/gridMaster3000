e384f9d09ba5033f665e34ce2bbfec6c4664adef
nven1 <neven.zdelar@gmail.com>
2018-11-09 10:57:47 +0100

    0.0.0.0.0.0.001

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
     $(".z0id0").ready(function productController() {
-        var mainGridRowCount = 10;
-        var mainGridColumnCount = 10;
-        var selectedElement;
+        var mainGridRowCount = 1;
+        var mainGridColumnCount = 15;
+        var selectedElement = null;
         createGrid()
 
         $( "#backColorBlack" ).click(function() {
@@ -19,12 +19,22 @@ $(document).ready(function(){
         
 
         $(".z0id0 div").click(function(){
+            if (selectedElement!=null) {
+                $("#"+selectedElement).get(0).style.setProperty("outline", "none");
+            }
+            var previousItem = selectedElement;
             selectedElement = this.id;
-            console.log(selectedElement);
+
+            if (previousItem==this.id) {
+                $("#"+selectedElement).get(0).style.setProperty("outline", "none");
+            } else {             
+                $(this).get(0).style.setProperty("outline-offset", "-2px");
+                $(this).get(0).style.setProperty("outline", "2px solid black");
+            }
+
         })
 
         function changeBackgroundColor(element, color) {
-            console.log();
             $("#"+element).get(0).style.setProperty("background-color", color);
         }
         /*
@@ -37,7 +47,9 @@ $(document).ready(function(){
             $(".z0id0").get(0).style.setProperty("grid-template-rows", mainGridRowCount*"1fr ");
             for (var r = 1; r<=mainGridRowCount; r++) {
                 for (var c = 1; c<=mainGridColumnCount; c++) {
-                    $(".z0id0").append("<div id='"+'z1id'+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+";'>aa</div>")
+                    $(".z0id0").append("<div id='"+'z1id'+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+"; width:100%; height:100%; min-height:100%; padding:0; min-width:100%;'>a</div>")
+                    $("#z1id"+r+c).resizable();
+                    //$(".z1id"+r+c).style.setProperty("resizable", "both");
                 }
             }
     
