ac20fa3288a4fb5779c0fb6ab70ed103a042fdc3
nven1 <neven.zdelar@gmail.com>
2018-11-13 00:59:58 +0100

    begginings of selection

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
     $(".z0id0").ready(function productController() {
-        var mainGridRowCount = 1;
+        var mainGridRowCount = 15;
         var mainGridColumnCount = 15;
         var selectedElement = null;
+        var mousedown=false;
+        var list=[];
         createGrid()
 
         $( "#backColorBlack" ).click(function() {
@@ -16,14 +18,15 @@ $(document).ready(function(){
             console.log(this);
             changeBackgroundColor(selectedElement, 'grey');
         });
-        
+   
 
-        $(".z0id0 div").click(function(){
+/*         $(".z0id0 div").on('mouseenter',function(){
             if (selectedElement!=null) {
                 $("#"+selectedElement).get(0).style.setProperty("outline", "none");
             }
             var previousItem = selectedElement;
             selectedElement = this.id;
+            list.push(selectedElement);
 
             if (previousItem==this.id) {
                 $("#"+selectedElement).get(0).style.setProperty("outline", "none");
@@ -31,8 +34,29 @@ $(document).ready(function(){
                 $(this).get(0).style.setProperty("outline-offset", "-2px");
                 $(this).get(0).style.setProperty("outline", "2px solid black");
             }
+            console.log(list);
+        }) */
 
+        $(".z0id0 div").on('mousedown',function(){
+            list=[];
+            mousedown = true;
+            selectedElement = this.id;
+            list.push(selectedElement);
+            
         })
+        $(".z0id0 div").on('mouseover',function(){
+            if(mousedown) {
+                selectedElement = this.id;
+                list.push(selectedElement);
+            }
+        })
+        $(".z0id0 div").on('mouseup',function(){
+            mousedown = false;
+            console.log(list);
+        })
+        $(".z0id0 div").disableSelection();
+
+        
 
         function changeBackgroundColor(element, color) {
             $("#"+element).get(0).style.setProperty("background-color", color);
@@ -47,9 +71,7 @@ $(document).ready(function(){
             $(".z0id0").get(0).style.setProperty("grid-template-rows", mainGridRowCount*"1fr ");
             for (var r = 1; r<=mainGridRowCount; r++) {
                 for (var c = 1; c<=mainGridColumnCount; c++) {
-                    $(".z0id0").append("<div id='"+'z1id'+r+c+"' style='background-color: aqua; grid-area:"+r+"/"+c+"; width:100%; height:100%; min-height:100%; padding:0; min-width:100%;'>a</div>")
-                    $("#z1id"+r+c).resizable();
-                    //$(".z1id"+r+c).style.setProperty("resizable", "both");
+                    $(".z0id0").append("<div id='"+'z1id'+r+c+"' style='background-color: lightgrey; grid-area:"+r+"/"+c+"; border: 1px solid black;'>a</div>")
                 }
             }
     
