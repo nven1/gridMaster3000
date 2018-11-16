732a71c1aa9e82b12d73bac8460143cfd163dbc8
nven1 <neven.zdelar@gmail.com>
2018-11-16 15:39:24 +0100

    taking shape, version 0.1!

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-        var mainGridRowCount = 30;
-        var mainGridColumnCount = 30;
+        var mainGridRowCount = 40;
+        var mainGridColumnCount = 40;
         var selectedElement = null;
         var mousedown=false;
         var rightClick = false;
         var list=[];
         var gridArea;
         var layer = 1;
-        var layer1Occupied=[];
+        var layer1Occupied=[]
+        var selectedGridArea;
+        var selectedGridAreaFirst;
+        var selectedGridAreaLast;
 
         createGrid()
+        $("#currentLayer").text(layer);
 
         $( "#backColorBlack" ).click(function() {
             changeBackgroundColor(selectedElement, 'black');
@@ -21,25 +25,26 @@ $(document).ready(function(){
             changeBackgroundColor(selectedElement, 'grey');
         });   
         $( "#layerUp" ).on('click', function(){
-            layer++;
-            $(".gridTile").css('z-index', layer);
-            console.log(layer);
-            
+            if (layer!=9) {
+                layer++;
+                $(".gridTile").css('z-index', layer);
+                $("#temp").css('z-index', layer);
+                $("#currentLayer").text(layer);
+            }
         }); 
         $( "#layerDown" ).on('click', function(){
-            if (layer!=0) {
-                
+            if (layer!=1) {
                 layer--;
                 $(".gridTile").css('z-index', layer);
+                $("#temp").css('z-index', layer);
+                $("#currentLayer").text(layer);
             }    
-            console.log(layer);
-            
         }); 
         $( "#resetSelection").on('click',resetSelection);
 
             
         $("#makeDiv").click(function() {
-            let rowList = [];
+/*             let rowList = [];
             let colList = [];
             // get coordinates of selected divs in list and give them to div
             for (let i=0; i<list.length; i++) {    
@@ -57,29 +62,28 @@ $(document).ready(function(){
                 rowList.push(row);
                 colList.push(col);
             }
-            // collision check
-/*             for (let i=0; i<rowList.length;i++) {
+            // collision check - napra
+            for (let i=0; i<rowList.length;i++) {
 
-            } */
+            }
 
             let minRow = Math.min.apply( Math, rowList );
             let maxRow = Math.max.apply( Math, rowList )+1;
             let minCol = Math.min.apply( Math, colList );
             let maxCol = Math.max.apply( Math, colList )+1;
-            gridArea = minRow+'/'+minCol+'/'+maxRow+'/'+maxCol
-            createDiv(gridArea);
-            console.log(layer1Occupied);
+            gridArea = minRow+'/'+minCol+'/'+maxRow+'/'+maxCol */
+            createDiv();
+            hideSelectionGrid();
+            //console.log(layer1Occupied);
         });   
         $(".main div").on('mousedown',function(){
             selectedElement = this.id;
             switch (event.which) {
                 case 1:
                     mousedown = true;
-                    if (list.includes(selectedElement) === false) {
-                        $(this).addClass("elementSelectClass");
-                        console.log("a");
-                        list.push(selectedElement);
-                    }
+                    let row = Number(this.id.split("r").pop().split("c").shift());
+                    let col = Number(this.id.split("r").pop().split("c").pop());
+                    selectedGridAreaFirst=row+'/'+col+'/';
                     break;
                 case 3:
                     rightClick = true;
@@ -93,10 +97,12 @@ $(document).ready(function(){
         $(".main div").on('mouseover',function(){
             selectedElement = this.id;
             if(mousedown) {
-                if (list.includes(selectedElement) === false) {
-                    $(this).addClass("elementSelectClass");
-                    list.push(selectedElement);
-                }
+                //let coo = this.id.split("r").pop();
+                let row = Number(this.id.split("r").pop().split("c").shift());
+                let col = Number(this.id.split("r").pop().split("c").pop());
+                if (row!=1) { row+=1; }
+                if (col!=1) { col+=1; }
+                showSelectionGrid(selectedGridAreaFirst+row+'/'+col);
             }
             else if (rightClick) {
                 console.log("eyyy");
@@ -125,10 +131,14 @@ $(document).ready(function(){
 
         $(document).on('mousedown','.userMade',function(){
             selectedElement = this.id;
+            $("#elementLayer").text("Layer: "+$(this).css('z-index'))
+            $("#elementBackColor").text("Background Color: "+$(this).css('background-color'))
+            $("#elementColor").text("Text Color: "+$(this).css('color'))
             
         })
         $(document).on('mouseup',function(){
             mousedown = false;
+            
             list.sort();
             console.log(list);
         })
@@ -144,22 +154,31 @@ $(document).ready(function(){
            this.z = z;
         }
         function createGrid() {
-            $(".main").get(0).style.setProperty("grid-template-columns", mainGridColumnCount*"1fr ");
-            $(".main").get(0).style.setProperty("grid-template-rows", mainGridRowCount*"1fr ");
+            $(".main").get(0).style.setProperty("grid-template-columns", mainGridColumnCount*"minmax(0, 1fr) ");
+            $(".main").get(0).style.setProperty("grid-template-rows", mainGridRowCount*"minmax(0, 1fr) ");
+            $(".topRuler").get(0).style.setProperty("grid-template-columns", 'repeat('+mainGridColumnCount+', 1fr)');
+            $(".leftRuler").get(0).style.setProperty("grid-template-rows", 'repeat('+mainGridRowCount+', 1fr)');
+            
             for (var r = 1; r<=mainGridRowCount; r++) {
+                $(".leftRuler").append("<span id='rulerLeft"+r+"' class='rulerLeftNumber' style='grid-area:"+r+"/1; z-index:1'>"+r+"</span>")
                 for (var c = 1; c<=mainGridColumnCount; c++) {
-                    $(".main").append("<div id='r"+r+"c"+c+"' class='gridTile' style='background-color: #ffffff00; grid-area:"+r+"/"+c+"; border: 1px solid #00000010;'></div>")
+                    $(".main").append("<div id='r"+r+"c"+c+"' class='gridTile' style='background-color: #ffffff00; grid-area:"+r+"/"+c+"; border: 1px solid #00000010; z-index:1'></div>")
                 }
             }
+            for (var c = 1; c<=mainGridColumnCount; c++) {
+                $(".topRuler").append("<span  id='rulerTop"+c+"'  class='rulerTopNumber'  style='grid-area:1/"+c+"; z-index:1'>"+c+"</span>")            }
         }
-        function createDiv(gridArea) {
-            $(".main").append("<div class='userMade' style='grid-area:"+gridArea+"; z-index:"+layer+"; background-color:#555; border-radius: 10px;'></div>")
-            resetSelection();
-            list=[];
-            gridArea=null;
+        function createDiv() {
+            $(".main").append("<div class='userMade' style='grid-area:"+selectedGridArea+"; z-index:"+layer+"; background-color:#"+layer+"aa; border-radius: 2px; color:#fff;'>"+layer+"</div>")
+            
         }
-        function resetSelection() {
-            $(".main div").removeClass("elementSelectClass");
-            list=[];
+        function showSelectionGrid(gridArea) {
+            $("#temp").css("grid-area",gridArea);
+            $("#temp").fadeIn();
+            selectedGridArea = gridArea;
+
+        }
+        function hideSelectionGrid() {
+            $("#temp").fadeOut();
         }
 });
